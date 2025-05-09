770cab59c7f6d3ca28ad7e17e9e7d07304b2fd60
doyouknowanything <andreevkorn@yandex.ru>
2025-05-09 13:51:32 +0300

    edited text of build number

--- task1/app/main.cpp
+++ task1/app/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int, char **)
 {
-	std::cout << "Version: " << VersionChecker::version() << std::endl;
+	std::cout << "build: " << VersionChecker::version() << std::endl;
 	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
